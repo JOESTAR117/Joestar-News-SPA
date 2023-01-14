b74b7ce510784d6204b660b8f920c1e3d1f3c16f
Jackson Rios <juniorrios1231@gmail.com>
2023-01-14 16:39:26 -0300

    s

---
+++ src/Datas.js
@@ -0,0 +1,30 @@
+export const news = [
+  {
+    title: "Notice 1",
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of",
+    image: "https://fakeimg.pl/440x320/282828/eae0d0/?retina=1",
+    likes: 2,
+    Comments: 5,
+  },
+  {
+    title: "Notice 2",
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of",
+    image: "https://fakeimg.pl/440x320/282828/eae0d0/?retina=1",
+    likes: 2,
+    Comments: 5,
+  },
+  {
+    title: "Notice 3",
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of",
+    image: "https://fakeimg.pl/440x320/282828/eae0d0/?retina=1",
+    likes: 2,
+    Comments: 5,
+  },
+  {
+    title : "Notice 4",
+    text : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of",
+    image : "https://fakeimg.pl/440x320/282828/eae0d0/?retina=1",
+    likes : 2,
+    Comments : 5,
+}
+];
