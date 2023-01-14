674d70296bf3f784022748b181fd9f8655e2757f
Jackson Rios <juniorrios1231@gmail.com>
2023-01-14 17:13:56 -0300

    componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import Home from './pages/Home/Home'
 
 function App() {
 
   return (
-    <h1>Olá React</h1>
+    <Home />
   )
 }
 
