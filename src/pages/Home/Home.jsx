29e2b25778a86eeb74b7d6fed3f410ad1bae11a1
Jackson Rios <juniorrios1231@gmail.com>
2023-01-15 01:27:04 -0300

    create navbar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
-export default function Home(){
-    return (
-    <Navbar />
-    <h1>Ola my friend</h1>
+import { Navbar } from "../../components/Navbar/Navbar";
 
-    )
-    
+export default function Home() {
+  return (
+    <section>
+      <Navbar />
+      <h1>Ola my friend</h1>
+    </section>
+  );
 }
